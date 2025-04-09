90ffaf467a168d3da4e3ba1ee055bb7f36dfe975
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2025-04-09 13:52:07 -0600

    cleaned up code and added comments

--- src/main.py
+++ src/main.py
@@ -6,16 +6,14 @@ name = ["0.5mass", "0.7mass", "0.9mass", "1mass", "1.2mass", "1.4mass",
         "1.6mass", "1.8mass", "2mass", "2.2mass", "2.4mass", "2.6mass", "3mass", "4mass", 
         "5mass", "6mass", "7mass", "8mass", "9mass", "10mass"]
 
-desired_age = 2e9
-
-
+# fill the md array with MESA tracks using mesa_reader
 for i in range(len(name)):
   md.append(MesaData('gregsWork/work/LOGS/' + name[i] + '.data'))
 
-# iso.hr(md)
-iso.isochrone(md, 1e8, 'red')
-iso.isochrone(md, 3.16e8, 'orange')
-iso.isochrone(md, 1e9, 'green')
-iso.isochrone(md, 3.16e9, 'blue')
-iso.isochrone(md, 1e10, 'purple')
-iso.show_plot()
+# iso.hr(md) # plots a simple hr diagram
+iso.isochrone(md, 1e8, 'red')  # plots an isochrone for 100 megayears
+iso.isochrone(md, 3.16e8, 'orange')  # plots an isochrone for 316 megayears
+iso.isochrone(md, 1e9, 'green')  # plots an isochrone for 1 gigayear
+iso.isochrone(md, 3.16e9, 'blue')  # plots an isochrone for 3.16 gigayears
+iso.isochrone(md, 1e10, 'purple')  # plots an isochrone for 10 gigiyears
+iso.show_plot()  # shows the plots
