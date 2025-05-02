e74313d275da39bf27330f839e0543362e17e40a
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2025-05-02 09:18:44 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from mesa_reader import MesaData
 from mesa_isochrone import mesa_isochrone
 
 # Initialize and load data
-plotter = isoFns()
+plotter = mesa_isochrone()
 md = []
 name = ["0.5mass", "0.7mass", "0.9mass", "1mass", "1.2mass", "1.4mass", 
         "1.6mass", "1.8mass", "2mass", "2.2mass", "2.4mass", "2.6mass", 
@@ -14,11 +14,11 @@ for n in name:
 plotter.load_models(md)
 
 # Add isochrones
-plotter.plot_isochrone(1e8, 'red')
-plotter.plot_isochrone(3.16e8, 'orange')
-plotter.plot_isochrone(1e9, 'green')
-plotter.plot_isochrone(3.16e9, 'blue')
-plotter.plot_isochrone(1e10, 'purple')
+plotter.plot_isochrone(1e8, track_color='green')
+plotter.plot_isochrone(3.16e8, track_color='orange')
+plotter.plot_isochrone(1e9, track_color='green')
+plotter.plot_isochrone(3.16e9, track_color='blue')
+plotter.plot_isochrone(1e10, track_color='purple')
 
 # Show plot
 plotter.show()
