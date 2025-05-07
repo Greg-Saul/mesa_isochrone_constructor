3404e3a3757b45774ca15dae96ac61386d5626ce
Greg <gregory@saulid.com>
2025-05-07 09:46:38 -0600

    update main and mesa_isochrone to save images and use glob

--- src/main.py
+++ src/main.py
@@ -1,29 +1,40 @@
-#from mesa_reader import MesaData
+from mesa_reader import MesaData
 from mesa_isochrone import mesa_isochrone
 import sys
+import glob
+import time
 
 ## Local to M Joyce system 
-sys.path.append('/home/mjoyce/MESA/py_mesa_reader/')
-from mesa_reader import MesaData
+# sys.path.append('/home/mjoyce/MESA/py_mesa_reader/')
+# from mesa_reader import MesaData
+
+t0 = time.time()
 
 # Initialize and load data
 plotter = mesa_isochrone()
 md = []
-name = ["0.5mass", "0.7mass", "0.9mass", "1mass", "1.2mass", "1.4mass", 
-        "1.6mass", "1.8mass", "2mass", "2.2mass", "2.4mass", "2.6mass", 
-        "3mass", "4mass", "5mass", "6mass", "7mass", "8mass", "9mass", "10mass"]
 
-for n in name:
-    md.append(MesaData('data/' + n + '.data'))
+file_paths = glob.glob("../histories/*.data")
+file_paths.sort()
+
+print(len(file_paths))
+
+for path in file_paths:
+  md.append(MesaData(path))
 
 plotter.load_models(md)
 
 # Add isochrones
-plotter.plot_isochrone(1e8, track_color='green')
-plotter.plot_isochrone(3.16e8, track_color='orange')
-plotter.plot_isochrone(1e9, track_color='green')
-plotter.plot_isochrone(3.16e9, track_color='blue')
-plotter.plot_isochrone(1e10, track_color='purple')
+plotter.plot_isochrone(1e8, track_color='green', show_hr=False, resolution=1000)
+plotter.plot_isochrone(3.16e8, track_color='orange', show_hr=False, resolution=1000)
+plotter.plot_isochrone(1e9, track_color='green', show_hr=False, resolution=1000)
+plotter.plot_isochrone(3.16e9, track_color='blue', show_hr=False, resolution=1000)
+plotter.plot_isochrone(1e10, track_color='purple', show_hr=False, resolution=1000)
+
+t1 = time.time()
+
+print("created isochrone in", t1-t0, "seconds")
 
 # Show plot
-plotter.show()
+plotter.save(image_name="test2")
+# plotter.show()
