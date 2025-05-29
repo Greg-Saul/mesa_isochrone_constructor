babdda986678ed46dbbd10122011dde905bd022e
Greg <gregory@saulid.com>
2025-05-29 09:54:04 -0600

    data name changes and sample3 updates

--- src/main_sample3.py
+++ src/main_sample3.py
@@ -1,6 +1,6 @@
 ########################################################
 # This sample main is to show loading mesa data objects 
-# directly into arrays for isocrone generation. 
+# directly into arrays for isochrone generation. 
 # This is the slowest method of rapidly generating 
 # multiple isocrones. This saves the plot into 
 # sample3.png
@@ -22,16 +22,13 @@ md = []
 file_paths = glob.glob("./data/*.data")
 file_paths.sort()
 
-print(len(file_paths))
-
 for path in file_paths:
   md.append(MesaData(path))
 
 plotter.load_models(md)
 
 # Add isochrones to plot
-plotter.plot_evolutionary_tracks()
-plotter.plot_isochrone(1e8, track_color='red', show_hr=False, resolution=1000)
+plotter.plot_isochrone(1e8, track_color='red', resolution=1000)
 plotter.plot_isochrone(3.16e8, track_color='orange', show_hr=False, resolution=1000)
 plotter.plot_isochrone(1e9, track_color='green', show_hr=False, resolution=1000)
 plotter.plot_isochrone(3.16e9, track_color='blue', show_hr=False, resolution=1000)
